3562f7bfdc999fbd7a84e84c9f9cc1d328b98e3f
Coolzz <coolzzminecraft@gmail.com>
2026-07-10 10:47:18 +0800

    Initial project structure and shared template

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header>
+
+    <div class="logo">
+        <h1>Wildlife Emporium</h1>
+        <p>Discover. Learn. Protect.</p>
+    </div>
+
+</header>
---
+++ includes/navigation.php
@@ -0,0 +1,19 @@
+<nav>
+
+    <ul>
+
+        <li><a href="/wildlife-emporium/">Home</a></li>
+
+        <li><a href="/wildlife-emporium/animals/">Animals</a></li>
+
+        <li><a href="/wildlife-emporium/articles/">Articles</a></li>
+
+        <li><a href="/wildlife-emporium/quiz/">Quiz</a></li>
+
+        <li><a href="/wildlife-emporium/contact/">Contact</a></li>
+
+        <li><a href="/wildlife-emporium/account/login.php">Login</a></li>
+
+    </ul>
+
+</nav>
