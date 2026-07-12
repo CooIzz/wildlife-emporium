ca36fb0110ca46d1e8d362e9efbcf13c157fc957
Bavan Kanesan <bavan.kanesan@gmail.com>
2026-07-12 08:17:08 +0800

    Add website logo and update header and quiz index

--- quiz/index.php
+++ quiz/index.php
@@ -15,7 +15,21 @@
 <?php include("../includes/navigation.php"); ?>
 
 <main>
+<h1>Quiz Domain</h1>
+<p>
+    Welcome to the quiz section of this website. Here, you will be tested on the profound knowledge
+    you have accumulated throughout your journey of learning everything there is to know about 
+    the Wildlife.
+</p>
 
+<form action="" method="post">
+<fieldset>
+<legend></legend>
+
+
+
+</fieldset>
+</form>
 </main>
 
 <?php include("../includes/footer.php"); ?>
