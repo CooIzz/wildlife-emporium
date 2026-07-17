cc8a7076d0b6294df4928fd1d2e73b5fa51b74bc
Bavan Kanesan <bavan.kanesan@gmail.com>
2026-07-17 13:03:29 +0800

    Updated the index page of the quiz section

--- quiz/index.php
+++ quiz/index.php
@@ -19,12 +19,24 @@
 <p>
     Welcome to the quiz section of this website. Here, you will be tested on the profound knowledge
     you have accumulated throughout your journey of learning everything there is to know about 
-    the Wildlife.
+    the Wildlife. Are you brave enough to accept this challenge and earn your place among the Wildlife 
+    experts? 
 </p>
 
 <p>
+    If you wish to partake in our quiz, please login(or register for newcomers) before proceeding.
+</p>
+
+<div class="quiz-wrapper">
+
+    <h2>Wildlife Quiz</h2>
+    <p></p>
+
+</div>
+
+<p>
     Which topic would you like to quiz yourself on?
-    <span>(Please select a topic):</span>
+    <span>(Please select a specific topic and you will be redirected to a different page):</span>
 </p>
 
 <ol>
