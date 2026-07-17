5f48c48d2b27b7b9f014775fbe9591c4feab4126
Bavan Kanesan <bavan.kanesan@gmail.com>
2026-07-17 14:05:29 +0800

    Improvements to quiz section

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <header>
 
-    <div class="logo">
+    <div class="logo" id="webLogo">
         <h1>Wildlife Emporium</h1>
         <p>Discover. Learn. Protect.</p>
-        <img src="../images/websitelogo.png">
+        <img src="../images/websitelogo.png" height=500px width=1000px>
     </div>
 
 </header>
--- quiz/index.php
+++ quiz/index.php
@@ -14,18 +14,37 @@
 <?php include("../includes/header.php"); ?>
 <?php include("../includes/navigation.php"); ?>
 
-<main>
-<h1>Quiz Domain</h1>
+<main id="quizIndex">
+<div><h1>Quiz Domain</h1></div>
 <p>
     Welcome to the quiz section of this website. Here, you will be tested on the profound knowledge
     you have accumulated throughout your journey of learning everything there is to know about 
     the Wildlife.
 </p>
 
+<br>
+<hr>
+<br>
+
 <p>
     Which topic would you like to quiz yourself on?
     <span>(Please select a topic):</span>
 </p>
+
+<br>
+
+<ol>
+<li><a>Topic 1: </a></li>
+<li><a>Topic 2: </a></li>
+<li><a>Topic 3: </a></li>
+<li><a>Topic 4: </a></li>
+<li><a>Topic 5: </a></li>
+<li><a>Topic 6: </a></li>
+<li><a>Topic 7: </a></li>
+<li><a>Topic 8: </a></li>
+<li><a>Topic 9: </a></li>
+<li><a>Topic 10: </a></li>
+</ol>
 
 
 </main>
