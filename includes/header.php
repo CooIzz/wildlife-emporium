67af475afe9fc0d9b135652ef9da55f88f23407e
Coolzz <coolzzminecraft@gmail.com>
2026-08-05 16:45:05 +0800

    reworked navbar

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,21 @@
 <header>
 
-    <div class="logo" id="webLogo">
-        <h1>Wildlife Emporium</h1>
-        <p>Discover. Learn. Protect.</p>
+    <div class="header-container">
+
+        <a href="/wildlife-emporium/" class="header-logo">
+
+            <img src="/wildlife-emporium/images/home-logo-test.svg" alt="Wildlife Emporium Logo">
+
+            <div class="header-logo-text">
+
+                <h1>Wildlife Emporium</h1>
+
+                <p>Discover. Learn. Protect.</p>
+
+            </div>
+
+        </a>
+
     </div>
 
 </header>
--- includes/navigation.php
+++ includes/navigation.php
@@ -1,19 +1,71 @@
 <nav>
 
-    <ul>
+    <div class="navigation-container">
 
-        <li><a href="/wildlife-emporium/">Home</a></li>
+        <ul class="navigation-links">
 
-        <li><a href="/wildlife-emporium/animals/">Animals</a></li>
+            <li>
+                <a href="/wildlife-emporium/">
+                    Home
+                </a>
+            </li>
 
-        <li><a href="/wildlife-emporium/articles/">Articles</a></li>
+            <li>
+                <a href="/wildlife-emporium/animals/">
+                    Animals
+                </a>
+            </li>
 
-        <li><a href="/wildlife-emporium/quiz/">Quiz</a></li>
+            <li>
+                <a href="/wildlife-emporium/articles/">
+                    Articles
+                </a>
+            </li>
 
-        <li><a href="/wildlife-emporium/contact/">Contact</a></li>
+            <li>
+                <a href="/wildlife-emporium/quiz/">
+                    Quiz
+                </a>
+            </li>
 
-        <li><a href="/wildlife-emporium/account/login.php">Login</a></li>
+            <li>
+                <a href="/wildlife-emporium/contact/">
+                    Contact
+                </a>
+            </li>
 
-    </ul>
+        </ul>
+
+        <div class="navigation-dropdown">
+
+            <button class="navigation-account-button">
+
+                <span>
+                    Account
+                </span>
+
+                <div class="navigation-avatar">
+
+                </div>
+
+            </button>
+
+            </button>
+
+            <div class="navigation-dropdown-menu">
+
+                <a href="/wildlife-emporium/account/login.php">
+                    Login
+                </a>
+
+                <a href="/wildlife-emporium/account/register.php">
+                    Register
+                </a>
+
+            </div>
+
+        </div>
+
+    </div>
 
 </nav>
