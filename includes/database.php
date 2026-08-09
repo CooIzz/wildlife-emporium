e6b580cc2febb44224aba1217a7547c4c66a5d91
Coolzz <coolzzminecraft@gmail.com>
2026-08-09 21:16:39 +0800

    e

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,5 @@
 <?php
 
-// This file_exists check is for Jason's and Bavan's local computer SQL running on port 3308.
-// Jason and Bavan will use the local file database.local.php that specifies port 3308 instead,
-// without changing the code here in database.php.
-// If you are using the default port 3306,
-// just use database.php normally.
-
 if (file_exists(__DIR__ . '/database.local.php')) {
     include_once __DIR__ . '/database.local.php';
 } else {
