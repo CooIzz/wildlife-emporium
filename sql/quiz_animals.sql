b7d09e76ec79905749fa50c5c9e139338e476d03
Bavan Kanesan <bavan.kanesan@gmail.com>
2026-09-05 14:04:07 +0800

    Updated sql section

--- sql/quiz_animals.sql
+++ sql/quiz_animals.sql
@@ -50,16 +50,16 @@ CREATE TABLE IF NOT EXISTS `quiz_animals` (
 --
 
 INSERT INTO `quiz_animals` (`id`, `topic_id`, `topic_num`, `animal_name`, `js_id`, `image_path`, `image_alt`, `easy_url`, `medium_url`, `difficult_url`) VALUES
-(1, 'Topic1', 1, 'African Lion', 'africanLion', '../images/AfricanLion.jpg', 'A picture of African Lion', './quiz_page.php?animal_id=1&difficulty=easy', './quiz_page.php?animal_id=1&difficulty=medium', './quiz_page.php?animal_id=1&difficulty=difficult'),
-(2, 'Topic2', 2, 'Orang Utan', 'orangUtan', '../images/OrangUtan.jpg', 'A picture of Orang Utan', './quiz_page.php?animal_id=2&difficulty=easy', './quiz_page.php?animal_id=2&difficulty=medium', './quiz_page.php?animal_id=2&difficulty=difficult'),
-(3, 'Topic3', 3, 'Penguin', 'penguin', '../images/Penguin.jpeg', 'A picture of Penguin', './quiz_page.php?animal_id=3&difficulty=easy', './quiz_page.php?animal_id=3&difficulty=medium', './quiz_page.php?animal_id=3&difficulty=difficult'),
-(4, 'Topic4', 4, 'Tiger', 'tiger', '../images/tiger.jpg', 'A picture of Tiger', './quiz_page.php?animal_id=4&difficulty=easy', './quiz_page.php?animal_id=4&difficulty=medium', './quiz_page.php?animal_id=4&difficulty=difficult'),
-(5, 'Topic5', 5, 'Giant Panda', 'giantPanda', '../images/panda.jpg', 'A picture of Giant Panda', './quiz_page.php?animal_id=5&difficulty=easy', './quiz_page.php?animal_id=5&difficulty=medium', './quiz_page.php?animal_id=5&difficulty=difficult'),
-(6, 'Topic6', 6, 'Raccoon', 'raccoon', '../images/raccoon.jpg', 'A picture of Raccoon', './quiz_page.php?animal_id=6&difficulty=easy', './quiz_page.php?animal_id=6&difficulty=medium', './quiz_page.php?animal_id=6&difficulty=difficult'),
-(7, 'Topic7', 7, 'Snow Leopard', 'snowLeopard', '../images/SnowLeopard.jpg', 'A picture of Snow Leopard', './quiz_page.php?animal_id=7&difficulty=easy', './quiz_page.php?animal_id=7&difficulty=medium', './quiz_page.php?animal_id=7&difficulty=difficult'),
-(8, 'Topic8', 8, 'Polar Bear', 'polarBear', '../images/PolarBear.jpg', 'A picture of Polar Bear', './quiz_page.php?animal_id=8&difficulty=easy', './quiz_page.php?animal_id=8&difficulty=medium', './quiz_page.php?animal_id=8&difficulty=difficult'),
-(9, 'Topic9', 9, 'Lynx', 'lynx', '../images/lynx.jpg', 'A picture of Lynx', './quiz_page.php?animal_id=9&difficulty=easy', './quiz_page.php?animal_id=9&difficulty=medium', './quiz_page.php?animal_id=9&difficulty=difficult'),
-(10, 'Topic10', 10, 'Cheetah', 'cheetah', '../images/cheetah.jpg', 'A picture of Cheetah', './quiz_page.php?animal_id=10&difficulty=easy', './quiz_page.php?animal_id=10&difficulty=medium', './quiz_page.php?animal_id=10&difficulty=difficult');
+(1, 'Topic1', 1, 'African Lion', 'africanLion', '../images/quiz/AfricanLion.jpg', 'A picture of African Lion', './quiz_page.php?animal_id=1&difficulty=easy', './quiz_page.php?animal_id=1&difficulty=medium', './quiz_page.php?animal_id=1&difficulty=difficult'),
+(2, 'Topic2', 2, 'Orang Utan', 'orangUtan', '../images/quiz/OrangUtan.jpg', 'A picture of Orang Utan', './quiz_page.php?animal_id=2&difficulty=easy', './quiz_page.php?animal_id=2&difficulty=medium', './quiz_page.php?animal_id=2&difficulty=difficult'),
+(3, 'Topic3', 3, 'Penguin', 'penguin', '../images/quiz/Penguin.jpeg', 'A picture of Penguin', './quiz_page.php?animal_id=3&difficulty=easy', './quiz_page.php?animal_id=3&difficulty=medium', './quiz_page.php?animal_id=3&difficulty=difficult'),
+(4, 'Topic4', 4, 'Tiger', 'tiger', '../images/quiz/tiger.jpg', 'A picture of Tiger', './quiz_page.php?animal_id=4&difficulty=easy', './quiz_page.php?animal_id=4&difficulty=medium', './quiz_page.php?animal_id=4&difficulty=difficult'),
+(5, 'Topic5', 5, 'Giant Panda', 'giantPanda', '../images/quiz/panda.jpg', 'A picture of Giant Panda', './quiz_page.php?animal_id=5&difficulty=easy', './quiz_page.php?animal_id=5&difficulty=medium', './quiz_page.php?animal_id=5&difficulty=difficult'),
+(6, 'Topic6', 6, 'Raccoon', 'raccoon', '../images/quiz/raccoon.jpg', 'A picture of Raccoon', './quiz_page.php?animal_id=6&difficulty=easy', './quiz_page.php?animal_id=6&difficulty=medium', './quiz_page.php?animal_id=6&difficulty=difficult'),
+(7, 'Topic7', 7, 'Snow Leopard', 'snowLeopard', '../images/quiz/SnowLeopard.jpg', 'A picture of Snow Leopard', './quiz_page.php?animal_id=7&difficulty=easy', './quiz_page.php?animal_id=7&difficulty=medium', './quiz_page.php?animal_id=7&difficulty=difficult'),
+(8, 'Topic8', 8, 'Polar Bear', 'polarBear', '../images/quiz/PolarBear.jpg', 'A picture of Polar Bear', './quiz_page.php?animal_id=8&difficulty=easy', './quiz_page.php?animal_id=8&difficulty=medium', './quiz_page.php?animal_id=8&difficulty=difficult'),
+(9, 'Topic9', 9, 'Lynx', 'lynx', '../images/quiz/lynx.jpg', 'A picture of Lynx', './quiz_page.php?animal_id=9&difficulty=easy', './quiz_page.php?animal_id=9&difficulty=medium', './quiz_page.php?animal_id=9&difficulty=difficult'),
+(10, 'Topic10', 10, 'Cheetah', 'cheetah', '../images/quiz/cheetah.jpg', 'A picture of Cheetah', './quiz_page.php?animal_id=10&difficulty=easy', './quiz_page.php?animal_id=10&difficulty=medium', './quiz_page.php?animal_id=10&difficulty=difficult');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
